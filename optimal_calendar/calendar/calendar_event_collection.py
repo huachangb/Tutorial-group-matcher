68e0b8f6337c36a8bee88e0c3ad4f4d4728af201
hua chang bakker <huachang.bakker@hotmail.com>
2022-07-10 21:03:11 +0200

    add class for event calendar collection and tests

--- optimal_calendar/calendar/calendar_event_collection.py
+++ optimal_calendar/calendar/calendar_event_collection.py
@@ -10,11 +10,11 @@ from .calendar_event import CalendarEvent
 class CalendarEventCollection():
     """ Collection of CalendarEvents. 
     Examples: courses or practical seminar groups (see other files)
-    Abstract strucuture
+    Abstract structure
     """
     def __init__(
             self, 
-            title: str, 
+            title: str = "", 
             description: str = "", 
             collection_type: CCollectionTypes = CCollectionTypes.OTHER,
             collection_id: str = None
@@ -25,6 +25,26 @@ class CalendarEventCollection():
         self.type = collection_type
         self.collection_id = collection_id
         self.__events = [] # a.k.a. schedule
+
+        # declarations for __iter__
+        self.__n = 0
+        self.__max = 0
+    
+
+    def __iter__(self):
+        """ Allows iterating over class """
+        self.__n = 0
+        self.__max = len(self.__events)
+        return self
+
+    
+    def __next__(self):
+        """ Returns n-th event """
+        if self.__n < self.__max:
+            self.__n += 1
+            return self.__events[self.__n - 1]
+        else:
+            raise StopIteration
 
 
     @property
@@ -37,3 +57,11 @@ class CalendarEventCollection():
         """ Adds event to list of events """
         assert isinstance(cal_event, CalendarEvent), "Argument should be of type CalendarEvent"
         self.__events.append(cal_event)
+
+
+    def overlaps(self, cal_event: CalendarEvent) -> bool:
+        """ 
+        Returns True if <cal_event> has overlap with any of the events of the current instance.
+        Else, returns False 
+        """
+        return any(event.overlaps(cal_event) for event in self.__events)
---
+++ optimal_calendar/tests/test_calendar/test_calendar_event_collection.py
@@ -0,0 +1,61 @@
+import pytest
+
+from ...calendar.calendar_event import CalendarEvent
+from ...calendar.calendar_event_collection import CalendarEventCollection
+from ...calendar.constants import CCollectionTypes
+from datetime import datetime
+
+class TestCalendarEventCollection():
+    def test__init__(self) -> None:
+        title = "test"
+        coll = CalendarEventCollection(
+            title=title
+        )
+        assert coll.title == title
+        assert coll.type == CCollectionTypes.OTHER
+        assert len(coll.events) == 0
+        
+    
+    @pytest.mark.parametrize("cal_events", [
+        [
+            CalendarEvent(title="", begin_date=datetime(2020, 5, 17, 1, 0), hours=1, minutes=0), 
+            CalendarEvent(title="", begin_date=datetime(2020, 5, 17, 2, 0), hours=2, minutes=0), 
+            CalendarEvent(title="", begin_date=datetime(2020, 5, 17, 1, 0), hours=0, minutes=5), 
+            CalendarEvent(title="", begin_date=datetime(2020, 5, 17, 2, 0), hours=2, minutes=0), 
+            CalendarEvent(title="", begin_date=datetime(2020, 5, 17, 1, 0), hours=2, minutes=0), 
+            CalendarEvent(title="", begin_date=datetime(2020, 5, 17, 2, 0), hours=2, minutes=0)
+        ]
+    ])
+    def test_add_event(self, cal_events: list) -> None:
+        coll = CalendarEventCollection(title="Test")
+
+        for index, cal_event in enumerate(cal_events):
+            coll.add_event(cal_event)
+            assert len(coll.events) == index + 1
+        
+
+    def test_overlaps(self) -> None:
+        events = [
+            CalendarEvent(title="", begin_date=datetime(2020, 5, 17, 1, 0), hours=1, minutes=0), 
+            CalendarEvent(title="", begin_date=datetime(2020, 5, 17, 2, 0), hours=2, minutes=0), 
+            CalendarEvent(title="", begin_date=datetime(2020, 5, 17, 1, 0), hours=0, minutes=5), 
+            CalendarEvent(title="", begin_date=datetime(2020, 5, 17, 2, 0), hours=2, minutes=0), 
+            CalendarEvent(title="", begin_date=datetime(2020, 5, 17, 1, 0), hours=2, minutes=0), 
+            CalendarEvent(title="", begin_date=datetime(2020, 5, 17, 2, 0), hours=2, minutes=0)
+        ]
+        coll = CalendarEventCollection()
+
+        for event in events:
+            coll.add_event(event)
+        
+        # overlap
+        overlap1 = CalendarEvent(title="", begin_date=datetime(2020, 5, 17, 1, 0), hours=0, minutes=20)
+        overlap2 = CalendarEvent(title="", begin_date=datetime(2020, 5, 17, 2, 0), hours=1, minutes=30)
+        assert coll.overlaps(overlap1)
+        assert coll.overlaps(overlap2)
+
+        # no overlap
+        no_overlap1 = CalendarEvent(title="", begin_date=datetime(2021, 5, 17, 1, 0), hours=0, minutes=20)
+        no_overlap2 = CalendarEvent(title="", begin_date=datetime(2022, 5, 17, 2, 0), hours=1, minutes=30)
+        assert not coll.overlaps(no_overlap1)
+        assert not coll.overlaps(no_overlap2)
